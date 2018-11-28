7bfb2c6cbf12fec2aef051105d4dc6249e693590
Obsiye <mrabdirizak@gmail.com>
2018-11-28 12:57:39 +0000

    Jokes can be stored in a Game class

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,13 +2,16 @@
 
 describe('Game', function(){
 	var game;
+	var mockJoke;
 
 	beforeEach(function(){
 		game = new Game();
+		mockJoke = jasmine.createSpyObj('joke', ['getLaughAmount']);
 	});
 
 	it('should store jokes',function(){
-
+		game.addJoke(mockJoke);
+		expect(game.getJokes().length).toEqual(1);
 	});
 
 });
--- src/game.js
+++ src/game.js
@@ -1,5 +1,13 @@
 'use strict'
 
 function Game(){
-    
+    this.jokes = []
+}
+
+Game.prototype.getJokes = function (joke) {
+   return this.jokes;
+}
+
+Game.prototype.addJoke = function(joke){
+    this.jokes.push(joke);
 }
