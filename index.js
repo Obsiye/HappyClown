dc9226415b0f467cdd024a362b954969fa3a44b3
Obsiye <mrabdirizak@gmail.com>
2018-11-29 13:57:04 +0000

    Heroku express server possible fix

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/stylesheets", express.static(__dirname, '/stylesheets'));
+app.use("/stylesheets", express.static(__dirname));
 app.use("/views", express.static(__dirname + '/views'));
 app.use("/src", express.static(__dirname + '/src'));
 
 // viewed at based directory http://localhost:8080/
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'views/happy_clown.html'));
+    res.sendFile(path.join(__dirname + '/'));
 });
 
 app.listen(process.env.PORT || 8080);
