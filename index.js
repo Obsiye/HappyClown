cfba287ae3dd3d8d6e37ed462763a04b05b42260
Obsiye <mrabdirizak@gmail.com>
2018-11-29 13:44:42 +0000

    Heroku usage with express server file

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/stylesheets", express.static(__dirname, '/stylesheets'));
+app.use("/views", express.static(__dirname + '/views'));
+app.use("/src", express.static(__dirname + '/src'));
+
+// viewed at based directory http://localhost:8080/
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + 'views/happy_clown.html'));
+});
+
+app.listen(process.env.PORT || 8080);
