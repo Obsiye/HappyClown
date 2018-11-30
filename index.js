913451ce020b1a7f5f91e20a75211d3ae2d3ca4e
Obsiye <mrabdirizak@gmail.com>
2018-11-30 09:33:50 +0000

    autocrlf false and use aws

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-var dataStore;
 const AWS = require('aws-sdk');
+var dataStore;
 
 app.use(express.static(path.join(__dirname)));
 app.use("/stylesheets", express.static(__dirname));
@@ -16,24 +16,24 @@ var params = {
     MaxKeys: 2
 };
 
+AWS.config.update({
+    accessKeyId: 'AKIAIS7IC77UFPGZ62WQ',
+    secretAccessKey: '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'
+});
+
 s3.listObjects(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else {
-        dataStore = data;
+        console.log(dataStore);
     }          // successful response
 });
 
 
 app.get('/', function (req, res) {
-    console.log(dataStore);
-    res.write(dataStore);
-    res.end();
-    // res.sendFile(path.join(__dirname + '/views/happy_clown.html'));
+    console.log(dataStore.jokes);
+    // res.write(dataStore);
+    // res.end();
+    res.sendFile(path.join(__dirname + '/views/happy_clown.html'));
 });
 
 app.listen(process.env.PORT || 8080);
-
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
