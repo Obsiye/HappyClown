ccfeb6f9eca8af24039f7aaf223787bc4180cb5f
AObsiye <mrabdirizak@gamil.com>
2018-11-30 19:43:50 +0000

    Test for data response from S3 Bucket

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use("/src", express.static(__dirname + '/src'));
 
 var s3 = new AWS.S3();
 
-var params = {
-    Bucket: "happyclownjokes",
-    MaxKeys: 2
-};
+// var params = {
+//     Bucket: "happyclownjokes",
+//     MaxKeys: 2
+// };
+
+var params = {Bucket: 'myBucket', Key: 'data.json'};
 
 s3.getObject(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
